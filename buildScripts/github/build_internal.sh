dc37a9cae987882d60afbdf05e12c177969919fe
MinyazevR <eeuriset8@gmail.com>
2024-07-23 06:45:05 +0300

    lol

--- buildScripts/github/build_internal.sh
+++ buildScripts/github/build_internal.sh
@@ -24,6 +24,7 @@ ccache -sz
 { which python && python -V || true ; }
 export PKG_CONFIG_PATH=$(python3.${TRIK_PYTHON3_VERSION_MINOR}-config --prefix)/lib/pkgconfig
 rm -f .qmake.cache
+find . -type f -exec file --mime-type {} \;
 { echo -n 'Remove broken object files ' ; find . -name '*.o' -type f -print0 | xargs -0 file --mime-type | grep -v 'application/x-object' | cut -f 1 -d ':' | xargs -t rm -f _RM_REQUIRES_ARGUMENTS_BUT_BSD_XARGS_IS_MISSING_R_PARAMETER_ARGUMENT || : ; echo Done ; }
 qmake -Wall PYTHON3_VERSION_MINOR=$TRIK_PYTHON3_VERSION_MINOR CONFIG+=$CONFIG $QMAKE_EXTRA $PROJECT.pro
 make -j $(nproc) qmake_all 2>&1 | tee -a build.log
