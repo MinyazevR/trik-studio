a31dd1000db391ab3f83b43a4a488c742b7ddc1b
MinyazevR <89993880+MinyazevR@users.noreply.github.com>
2024-07-30 13:06:24 +0300

    Update install_Linux.sh

--- buildScripts/github/install_Linux.sh
+++ buildScripts/github/install_Linux.sh
@@ -6,7 +6,6 @@ ID=$(grep '^ID=' /etc/os-release | cut -d'=' -f2)
 if [ "$ID" == "altlinux" ]; then
     # sudo bash -c 'echo "rpm [alt] http://ftp.altlinux.org/pub/distributions/ALTLinux/Sisyphus x86_64 classic" > /etc/apt/sources.list.d/sisyphus.list'
     apt-get update && apt-get install -y curl xz p7zip-standalone rsync libusb-devel python3-dev zlib-devel make ccache python3-module-pip
-    python3 -m pip install -U pip
     python3 -m pip install aqtinstall
     python3 -m aqt install-qt -O "$HOME/Qt" linux desktop "${TRIK_QT_VERSION}"
     python3 -m aqt install-tool -O "$HOME/Qt" linux desktop tools_ifw
