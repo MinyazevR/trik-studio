3dfa77406fa38dbe6ccdbe88fedd207a1461dac3
MinyazevR <89993880+MinyazevR@users.noreply.github.com>
2024-07-30 18:35:48 +0300

    Update install_Linux.sh

--- buildScripts/github/install_Linux.sh
+++ buildScripts/github/install_Linux.sh
@@ -4,11 +4,10 @@ set -euxo pipefail
 ID=$(grep '^ID=' /etc/os-release | cut -d'=' -f2)
 
 if [ "$ID" == "altlinux" ]; then
-    # sudo bash -c 'echo "rpm [alt] http://ftp.altlinux.org/pub/distributions/ALTLinux/Sisyphus x86_64 classic" > /etc/apt/sources.list.d/sisyphus.list'
-    apt-get update && apt-get install -y gcc-c++ curl xz p7zip-standalone rsync libusb-devel libudev-devel libGL-devel libGLX-mesa python3-dev zlib-devel make ccache python3-module-pip time
-    python3 -m pip install aqtinstall
-    python3 -m aqt install-qt -O "$HOME/Qt" linux desktop "${TRIK_QT_VERSION}" -m qtscript
-    python3 -m aqt install-tool -O "$HOME/Qt" linux desktop tools_ifw
+    apt-get update && apt-get install -y gcc-c++ curl xz p7zip-standalone rsync libusb-devel \
+    libudev-devel libGL-devel libGLX-mesa python3-dev zlib-devel make ccache python3-module-pip time \
+    rsync qt5-multimedia-devel qt5-svg-devel qt5-script-devel qt5-tools
+    
 else
     docker pull trikset/linux-builder
     docker run --cap-add SYS_PTRACE -d -v $HOME:$HOME:rw -w `pwd` --name builder trikset/linux-builder Xvfb :0
