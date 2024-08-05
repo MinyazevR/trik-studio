231381fc1da4d17ebd923ea29f8e818dfdb3698f
MinyazevR <89993880+MinyazevR@users.noreply.github.com>
2024-08-05 19:02:54 +0300

    Update install_Linux.sh

--- buildScripts/github/install_Linux.sh
+++ buildScripts/github/install_Linux.sh
@@ -6,7 +6,7 @@ ID=$(grep '^ID=' /etc/os-release | cut -d'=' -f2)
 if [ "$ID" = "altlinux" ]; then
     apt-get update && apt-get install -y gcc-c++ curl xz p7zip-standalone rsync libusb-devel \
     libudev-devel libGL-devel libGLX-mesa python3-dev zlib-devel make ccache python3-module-pip time \
-    rsync qt5-multimedia-devel qt5-svg-devel qt5-script-devel qt5-tools
+    rsync qt5-multimedia-devel qt5-svg-devel qt5-script-devel qt5-tools qt5-serialport-devel
     
 else
     docker pull trikset/linux-builder
