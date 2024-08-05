c4e4ff5e06f3fa9c4706af1a44c052dc5b53807c
MinyazevR <89993880+MinyazevR@users.noreply.github.com>
2024-08-05 18:19:34 +0300

    Update install_Linux.sh

--- buildScripts/github/install_Linux.sh
+++ buildScripts/github/install_Linux.sh
@@ -3,7 +3,7 @@ set -euxo pipefail
 
 ID=$(grep '^ID=' /etc/os-release | cut -d'=' -f2)
 
-if [ "$ID" == "altlinux" ]; then
+if [ "$ID" = "altlinux" ]; then
     apt-get update && apt-get install -y gcc-c++ curl xz p7zip-standalone rsync libusb-devel \
     libudev-devel libGL-devel libGLX-mesa python3-dev zlib-devel make ccache python3-module-pip time \
     rsync qt5-multimedia-devel qt5-svg-devel qt5-script-devel qt5-tools
