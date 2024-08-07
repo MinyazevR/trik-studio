5f8c14ecfdad98fc3eede22c5ecd2e9a130b6f29
MinyazevR <eeuriset8@gmail.com>
2024-08-07 16:26:21 +0300

    lolol

--- buildScripts/github/install_Linux.sh
+++ buildScripts/github/install_Linux.sh
@@ -6,7 +6,7 @@ ID=$(grep '^ID=' /etc/os-release | cut -d'=' -f2)
 if [ "$ID" = "altlinux" ]; then
     apt-get update && apt-get install -y gcc-c++ curl xz p7zip-standalone rsync libusb-devel \
     libudev-devel libGL-devel libGLX-mesa python3-dev zlib-devel make ccache python3-module-pip time \
-    rsync qt5-multimedia-devel qt5-svg-devel qt5-script-devel qt5-tools qt5-serialport-devel
+    qt5-default qt5-multimedia-devel qt5-svg-devel qt5-script-devel qt5-tools qt5-serialport-devel
 else
     docker pull trikset/linux-builder
     docker run --cap-add SYS_PTRACE -d -v $HOME:$HOME:rw -w `pwd` --name builder trikset/linux-builder Xvfb :0
