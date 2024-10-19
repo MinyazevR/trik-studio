c536a1e051b1df9ff611753e177a775ebaec193f
MinyazevR <eeuriset8@gmail.com>
2024-10-20 00:28:41 +0300

    lol

--- qrtranslations/fr/plugins/robots/trikKitInterpreterCommon_fr.ts
+++ qrtranslations/fr/plugins/robots/trikKitInterpreterCommon_fr.ts
@@ -238,7 +238,7 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location line="+58"/>
+        <location line="+61"/>
         <source>TRIK_PYTHONPATH must be set correctly to run Python script.</source>
         <translation type="unfinished"></translation>
     </message>
--- qrtranslations/ru/plugins/robots/trikKitInterpreterCommon_ru.ts
+++ qrtranslations/ru/plugins/robots/trikKitInterpreterCommon_ru.ts
@@ -307,7 +307,7 @@
         <translation>Оставновить</translation>
     </message>
     <message>
-        <location line="+58"/>
+        <location line="+61"/>
         <source>TRIK_PYTHONPATH must be set correctly to run Python script.</source>
         <translation>Для запуска программы на Python доложна быть корректно выставлена переменная окружения TRIK_PYTHONPATH</translation>
     </message>
