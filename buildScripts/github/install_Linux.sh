f5f4dd74860f7dfa524f9da25b63d019579a7d7d
MinyazevR <eeuriset8@gmail.com>
2024-10-22 23:24:42 +0300

    y

--- buildScripts/github/install_Linux.sh
+++ buildScripts/github/install_Linux.sh
@@ -6,16 +6,16 @@ BUILD_INSTALLER=${BUILD_INSTALLER:-false}
 QTIFW_VERSION=${QTIFW_VERSION:-4.6.1}
 GCC_VERSION=${GCC_VERSION:-13}
 
-if [ $ID = "altlinux" ]; then
+if [ "$ID" = "altlinux" ]; then
   apt-get update && apt-get install -y gcc-c++ curl xz p7zip-standalone rsync libusb-devel \
   libudev-devel libGL-devel libGLX-mesa python3-dev zlib-devel make ccache python3-module-pip time \
   qt5-multimedia-devel qt5-svg-devel qt5-script-devel qt5-tools qt5-serialport-devel
-elif [ $ID = "ubuntu" ]; then
+elif [ "$ID" = "ubuntu" ]; then
   sudo apt-get update && sudo apt-get install -y --no-install-recommends \
   ccache curl libusb-1.0-0-dev make qtscript5-dev qttools5-dev-tools qtmultimedia5-dev libqt5serialport5-dev libqt5svg5-dev \
   libudev-dev python3.${TRIK_PYTHON3_VERSION_MINOR}-dev qtbase5-private-dev
-elif [ $ID = "rocky" ]; then
-  yum install -y epel-release # for ccache and p7zip
+elif [ "$ID" = "rocky" ]; then
+  yum update -y && yum install -y epel-release
   yum install --allowerasing -y sudo which libusbx-devel curl wget make gcc-toolset-"${GCC_VERSION}"-{gcc-c++,libasan-devel,libubsan-devel,gdb} \
   git-core ccache zlib-devel rsync python3-{devel,pip,urllib3} mesa-libGL-devel systemd-devel fontconfig p7zip time
     
