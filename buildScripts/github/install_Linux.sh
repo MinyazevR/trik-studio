553ecaa12ee7be9a81c591c8b6785520ed97a1b1
MinyazevR <eeuriset8@gmail.com>
2024-10-22 18:04:49 +0300

    alef

--- buildScripts/github/install_Linux.sh
+++ buildScripts/github/install_Linux.sh
@@ -12,9 +12,10 @@ if [ "$ID" = "altlinux" ]; then
 elif [ "$ID" = "ubuntu" ]; then
     sudo apt-get update && sudo apt-get install -y --no-install-recommends vera++ \
     ccache curl libusb-1.0-0-dev make qtscript5-dev qttools5-dev-tools qtmultimedia5-dev libqt5serialport5-dev libqt5svg5-dev \
-    libudev-dev python3-dev python3
+    libudev-dev python3.${TRIK_PYTHON3_VERSION_MINOR}-dev qtbase5-private-dev
     
-    if ["$BUILD_INSTALLER" == "true"]; then
+    if [ "$BUILD_INSTALLER" = "true" ]; then
+       TRIK_PYTHON=python3.${TRIK_PYTHON3_VERSION_MINOR}
       "$TRIK_PYTHON" -m pip install -U pip
       "$TRIK_PYTHON" -m pip install aqtinstall
       "$TRIK_PYTHON" -m aqt install-tool -O "/opt/qtifw" linux desktop tools_ifw "${QTIFW_VERSION}"
