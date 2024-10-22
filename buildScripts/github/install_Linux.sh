80f90650340c351da15773eb1f519cbb3ed754c5
MinyazevR <eeuriset8@gmail.com>
2024-10-22 17:04:10 +0300

    kjjj

--- buildScripts/github/install_Linux.sh
+++ buildScripts/github/install_Linux.sh
@@ -2,6 +2,7 @@
 set -euxo pipefail
 
 ID=$(grep '^ID=' /etc/os-release | cut -d'=' -f2)
+TRIK_PYTHON=python3.${TRIK_PYTHON3_VERSION_MINOR}
 
 if [ "$ID" = "altlinux" ]; then
     apt-get update && apt-get install -y gcc-c++ curl xz p7zip-standalone rsync libusb-devel \
@@ -10,5 +11,6 @@ if [ "$ID" = "altlinux" ]; then
 elif [ "$ID" = "ubuntu" ]; then
     sudo apt-get update && sudo apt-get install -y --no-install-recommends vera++ \
     ccache curl libusb-1.0-0-dev make qtscript5-dev qttools5-dev-tools qtmultimedia5-dev libqt5serialport5-dev libqt5svg5-dev \
-    libudev-dev python3-dev
+    libudev-dev python3.${TRIK_PYTHON}-dev
 fi
+
