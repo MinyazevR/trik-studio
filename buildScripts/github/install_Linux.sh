c721d9edf8a060ab1a4882bf4895bd21ee1bd9c2
MinyazevR <eeuriset8@gmail.com>
2024-10-22 15:06:22 +0300

    lol

--- buildScripts/github/install_Linux.sh
+++ buildScripts/github/install_Linux.sh
@@ -6,8 +6,8 @@ ID=$(grep '^ID=' /etc/os-release | cut -d'=' -f2)
 if [ "$ID" = "altlinux" ]; then
     apt-get update && apt-get install -y gcc-c++ curl xz p7zip-standalone rsync libusb-devel \
     libudev-devel libGL-devel libGLX-mesa python3-dev zlib-devel make ccache python3-module-pip time \
-    rsync qt5-multimedia-devel qt5-svg-devel qt5-script-devel qt5-tools qt5-serialport-devel
+    qt5-multimedia-devel qt5-svg-devel qt5-script-devel qt5-tools qt5-serialport-devel
 elif [ "$ID" = "ubuntu" ]; then
     sudo apt-get update && sudo apt-get install -y --no-install-recommends vera++ qttools5-dev-tools \
-    qtbase5-dev
+    qtbase5-dev ccache curl libusb-dev make
 fi
