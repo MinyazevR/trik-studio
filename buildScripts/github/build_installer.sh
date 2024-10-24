0e9d91a957ad30dcf872cd095a8f4af9f29770bc
MinyazevR <eeuriset8@gmail.com>
2024-10-24 12:45:29 +0300

    d

--- buildScripts/github/build_installer.sh
+++ buildScripts/github/build_installer.sh
@@ -22,6 +22,14 @@ if $NEED_DEPLOY ; then
     $EXECUTOR bash -c "mkdir -p ~/.ssh && install -m 600 /dev/null ~/.ssh/id_rsa && echo \"$ssh_key\" > ~/.ssh/id_rsa"
 fi
           
+if [[ $RUNNER_OS == Linux ]] ; then
+      echo Start build checker archive
+      $EXECUTOR bash -c "bin/build-checker-installer.sh"
+      if $NEED_DEPLOY ; then
+          $EXECUTOR bash -c "rsync -v --rsh='ssh -o StrictHostKeyChecking=no' bin/trik_checker.tar.xz $username@$host:~/dl/ts/fresh/checker/checker-linux-$CONFIG-$BRANCH_NAME-$ID.tar.xz"
+      fi
+fi
+
 echo Start build installer
 $EXECUTOR bash -c "installer/build-trik-studio.sh $QTBIN $QTIFWBIN ."
 
