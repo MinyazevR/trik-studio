5b0ef596271343f8b5edbce2d8ddc7c2c1d3b372
MinyazevR <eeuriset8@gmail.com>
2024-10-25 17:25:19 +0300

    test

--- buildScripts/github/build_internal.sh
+++ buildScripts/github/build_internal.sh
@@ -11,9 +11,7 @@ ccache -sz
 { which python3 && python3 -V || true ; }
 { which python && python -V || true ; }
 export PYTHON_DIR=$(python3.${TRIK_PYTHON3_VERSION_MINOR}-config --prefix --embed)
-export PKG_CONFIG_PATH="$PYTHON_DIR/lib/pkgconfig"
-ls $PKG_CONFIG_PATH
-ls /usr/lib64/pkgconfig
+#export PKG_CONFIG_PATH="$PYTHON_DIR/lib64/pkgconfig"
 rm -f .qmake.cache
 qmake -Wall PYTHON_VERSION=3.$TRIK_PYTHON3_VERSION_MINOR PYTHON_PATH=/usr CONFIG+=$CONFIG $QMAKE_EXTRA $PROJECT.pro
 make -j $(nproc) qmake_all 2>&1 | tee -a build.log
