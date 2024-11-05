f5eb2fe2538efbd844911de8918e27fa746455db
MinyazevR <eeuriset8@gmail.com>
2024-11-05 15:25:14 +0300

    ff

--- buildScripts/github/install_installer_environment.sh
+++ buildScripts/github/install_installer_environment.sh
@@ -7,7 +7,7 @@ TRIK_PYTHON="python3.${TRIK_PYTHON3_VERSION_MINOR}"
 TRIK_QT_VERSION=${TRIK_QT_VERSION:-5.15}
 
 yum update -y && yum install -y epel-release
-yum install --allowerasing -y sudo which libusbx-devel curl wget make gcc-toolset-"$GCC_VERSION"-{gcc-c++,libasan-devel,libubsan-devel,gdb} \
+yum install --allowerasing -y sudo which libusbx-devel curl wget make gcc-toolset-"$GCC_VERSION"-gcc-c++ \
 git-core ccache zlib-devel rsync "$TRIK_PYTHON"-{devel,pip,urllib3} mesa-libGL-devel systemd-devel fontconfig p7zip xz time
 yum install -y libxkbcommon-x11 qt5-qtbase-gui
 yum install -y pulseaudio-libs-glib2
