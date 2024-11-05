11d32f603b3a752c0fba2f5f0c34c25f8ec2af71
MinyazevR <eeuriset8@gmail.com>
2024-11-05 22:26:41 +0300

    f

--- buildScripts/github/install_installer_Linux.sh
+++ buildScripts/github/install_installer_Linux.sh
@@ -7,10 +7,9 @@ if [ "$ID" = "altlinux" ]; then
   apt-get update && apt-get install -y libxkbcommon-x11 libX11 fontconfig libxcbutil-icccm \
   libxcbutil-image libdbus libxcbutil-keysyms libxcb-render-util
 elif [ "$ID" = "ubuntu" ]; then
-  apt-get update && apt-get install -y libxkbcommon-x11-0 libxcb-icccm4 libxcb-image0 \
+  sudo apt-get update && sudo apt-get install -y libxkbcommon-x11-0 libxcb-icccm4 libxcb-image0 \
   libxcb-keysyms1 libxcb-randr0 libxcb-render-util0 libxcb-sync1 libxcb-xfixes0 \
   libxcb-shape0 libx11-xcb1 libfreetype6 libfontconfig1 libdbus-1-3
-  
 elif [ "$ID" = "arch" ]; then
   pacman -Sy && pacman -S libxkbcommon-x11 xcb-util-wm xcb-util-image xcb-util-keysyms xcb-util-renderutil fontconfig libx11 --noconfirm
 elif [[ "$ID" = "rocky" || "$ID" = '"rocky"' ]]; then
@@ -31,10 +30,11 @@ LIB_DIR=/opt/TRIKStudio/lib && echo "LIB_DIR=$LIB_DIR" >> $GITHUB_ENV
 APP_DIR=/opt/TRIKStudio && echo "APP_DIR=$APP_DIR" >> $GITHUB_ENV
 
 export LD_LIBRARY_PATH="$LIB_DIR"
-"$BIN_DIR"/2D-model --version
-"$BIN_DIR"/checkapp --version
-"$BIN_DIR"/patcher --version
-"$APP_DIR"/maintenance --version
-"$APP_DIR"/trik-studio --version
+"$BIN_DIR"/2D-model --version --platform minimal
+"$BIN_DIR"/checkapp --version --platform minimal
+"$BIN_DIR"/patcher --version --platform minimal
+"$APP_DIR"/maintenance --version --platform minimal
+"$APP_DIR"/trik-studio --version --platform minimal
 
+cd "$LIB_DIR"
 ls *.so* | xargs ldd | grep "not found" || exit 0
