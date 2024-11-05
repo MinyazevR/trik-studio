9b51a8a86f96be455ee8425ecd7ba97e4832b7bf
MinyazevR <eeuriset8@gmail.com>
2024-11-05 20:35:02 +0300

    f

--- buildScripts/github/install_installer_environment.sh
+++ buildScripts/github/install_installer_environment.sh
@@ -8,8 +8,8 @@ TRIK_QT_VERSION=${TRIK_QT_VERSION:-5.15}
 
 yum update -y && yum install -y epel-release
 yum install --allowerasing -y sudo which libusbx-devel curl wget make gcc-toolset-"$GCC_VERSION"-{gcc-c++,libasan-devel,libubsan-devel,gdb} \
-git-core ccache zlib-devel rsync "$TRIK_PYTHON"-{devel,pip,urllib3} mesa-libGL-devel systemd-devel fontconfig p7zip xz time
-yum install -y libxkbcommon-x11 qt5-qtbase-gui
+git-core ccache zlib-devel rsync "$TRIK_PYTHON"-{devel,pip,urllib3} mesa-libGL-devel systemd-devel fontconfig p7zip xz time findutils
+yum install -y libxkbcommon-x11 qt5-qtbase-gui 
 yum install -y pulseaudio-libs-glib2
 echo "source scl_source enable gcc-toolset-$GCC_VERSION" >> ~/.bash_profile
 
