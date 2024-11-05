518252897562908bbbf9ea5fa32e74e166b3e07a
MinyazevR <eeuriset8@gmail.com>
2024-11-05 15:17:36 +0300

    f

--- buildScripts/github/install_installer_Linux.sh
+++ buildScripts/github/install_installer_Linux.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 set -euxo pipefail
 
+ID=$(grep '^ID=' /etc/os-release | cut -d'=' -f2)
+
 if [ "$ID" = "altlinux" ]; then
   apt-get update && apt-get install -y libxkbcommon-x11 libX11 fontconfig libxcbutil-icccm \
   libxcbutil-image libdbus libxcbutil-keysyms libxcb-render-util
-elif [ "$ID" = "ubuntu" ]; then
-  sudo apt-get update && sudo apt-get install -y --no-install-recommends \
-  ccache curl libusb-1.0-0-dev make qtscript5-dev qttools5-dev-tools qtmultimedia5-dev libqt5serialport5-dev libqt5svg5-dev \
-  libudev-dev python3.${TRIK_PYTHON3_VERSION_MINOR}-dev qtbase5-private-dev qtwayland5
+if [ "$ID" = "arch" ]; then
+  pacman -Sy && pacman -S libxkbcommon-x11 xcb-util-wm xcb-util-image xcb-util-keysyms \ 
+  xcb-util-renderutil fontconfig libx11 --noconfirm
 elif [[ "$ID" = "rocky" || "$ID" = '"rocky"' ]]; then
   yum update -y && yum install -y libxkbcommon-x11 xcb-util-wm libX11 xcb-util-image \
   dbus-libs xcb-util-keysyms xcb-util-renderutil fontconfig libX11-xcb
 fi
 
+INSTALLER_NAME="$INSTALLER_NAME.run"
+chmod +x "$INSTALLER_NAME"
 ./$INSTALLER_NAME --verbose --script trik_studio_installscript.qs --platform minimal
 echo "BIN_DIR=/opt/TRIKStudio/bin" >> $GITHUB_ENV
 echo "LIB_DIR=/opt/TRIKStudio/lib" >> $GITHUB_ENV
