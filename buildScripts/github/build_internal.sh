2f1690d1519407e7ae8e60b38dc620b98f06b752
MinyazevR <89993880+MinyazevR@users.noreply.github.com>
2024-11-22 19:56:20 +0300

    Update build_internal.sh

--- buildScripts/github/build_internal.sh
+++ buildScripts/github/build_internal.sh
@@ -12,6 +12,7 @@ ccache -sz
 { which python && python -V || true ; }
 export PYTHON_DIR=$(python3.${TRIK_PYTHON3_VERSION_MINOR}-config --prefix)
 rm -f .qmake.cache
+git -C $PWD -c 'safe.directory=*' describe --tags --always --dirty --abbrev=6 || true
 qmake -Wall PYTHON_VERSION=3.$TRIK_PYTHON3_VERSION_MINOR PYTHON_PATH=/usr CONFIG+=$CONFIG $QMAKE_EXTRA $PROJECT.pro
 make -j $(nproc) qmake_all 2>&1 | tee -a build.log
 ccache -s
