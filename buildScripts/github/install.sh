941895ac24964e50040eef95355998394d6d8487
MinyazevR <eeuriset8@gmail.com>
2024-12-25 16:37:30 +0300

    try another syntax

--- buildScripts/github/install.sh
+++ buildScripts/github/install.sh
@@ -63,9 +63,9 @@ case "`uname`" in
         qt5-qtsvg-devel qt5-qtbase-devel qt5-qtbase-private-devel qt5-qtwayland
       else
         #libQt5WaylandCompositor.so.5.15: libQt5Quick.so.5 libQt5Qml.so.5 libQt5QmlModels.so.5 
-        modules=("qtscript " "qtwaylandcompositor")
-        archives=("qtbase " "qtmultimedia" "qtsvg" "qtscript" "qttools" "qtserialport" "qtimageformats" "icu" "qtwayland" "qtdeclarative")
-        install_qt linux desktop "$TRIK_QT_VERSION" "$HOME/Qt" "${modules[@]}" "${archives[@]}"
+        modules="qtscript qtwaylandcompositor"
+        archives="qtbase qtmultimedia qtsvg qtscript qttools qtserialport qtimageformats icu qtwayland qtdeclarative"
+        install_qt linux desktop "$TRIK_QT_VERSION" "$HOME/Qt" "$modules" "$archives"
         QT_ROOT_DIR=$(ls -1d "$HOME"/Qt/$TRIK_QT_VERSION*/gcc_64 | head -n 1)
         echo "$QT_ROOT_DIR/bin" >> $GITHUB_PATH
       fi
