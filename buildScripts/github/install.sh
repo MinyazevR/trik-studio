426f278803bed0e7706bfa9dd0a692c70dd431b1
MinyazevR <eeuriset8@gmail.com>
2025-11-12 12:27:11 +0300

    j

--- buildScripts/github/install.sh
+++ buildScripts/github/install.sh
@@ -49,8 +49,6 @@ case "$(uname)" in
     MACPORTS_URL="https://github.com/macports/macports-base/releases/download/v2.11.6/${MACPORTS_PKG}"
     
     curl -L --fail --output "${MACPORTS_PKG}" "${MACPORTS_URL}"
-    
-    curl -O https://github.com/macports/macports-base/releases/download/v2.11.6/MacPorts-2.11.6-14-Sonoma.pkg
     sudo installer -pkg "${MACPORTS_PKG}" -target /
     sudo port selfupdate
     arch -x86_64 sudo port install libusb
