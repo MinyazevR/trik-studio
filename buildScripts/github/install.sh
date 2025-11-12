f50c152d99a48e047c9b1ac2b49f108b5915e6d8
MinyazevR <eeuriset8@gmail.com>
2025-11-12 12:32:33 +0300

    G

--- buildScripts/github/install.sh
+++ buildScripts/github/install.sh
@@ -50,6 +50,7 @@ case "$(uname)" in
     
     curl -L --fail --output "${MACPORTS_PKG}" "${MACPORTS_URL}"
     sudo installer -pkg "${MACPORTS_PKG}" -target /
+    echo "/opt/local/bin" >> $GITHUB_PATH
     sudo port selfupdate
     arch -x86_64 sudo port install libusb
     
