d4d2e6fe6e02ce81dbef7aabb7e93099a1424dc8
MinyazevR <eeuriset8@gmail.com>
2026-07-06 23:11:42 +0300

    V

--- buildScripts/github/run_tests.sh
+++ buildScripts/github/run_tests.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -euo pipefail
+set -uo
 
 export QT_QPA_PLATFORM=${QT_QPA_PLATFORM:-minimal}
 
@@ -14,8 +14,18 @@ env
 
 PYTHONQT_RUN_ONLY_MEMORY_TESTS="true" make check -k -s
 pushd "bin"
-for i in {1..100}; do strace -f ./trik-v62-qts-generator-tests; done
 
+for i in {1..100}; do
+    echo "Run #$i"
+
+    strace -f -o strace.log ./trik-v62-qts-generator-tests
+    ret=$?
+    cat strace.log
+    ((ret == 0)) || break
+done
+
+сat strace.log
+ 
 eval "$TESTS" && popd
 
 [ -r tests_qrs.7z ] || curl -Lo tests_qrs.7z https://dl.trikset.com/edu/.solutions20200701/testing_small.7z
